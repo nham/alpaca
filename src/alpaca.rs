6bede68d49b6525d65a3e565eee4fe15e05affdb
nham <hamann.nick@gmail.com>
2014-07-31 03:08:07 -0400

    Add initial work on matching input params to a program

--- src/alpaca.rs
+++ src/alpaca.rs
@@ -21,6 +21,13 @@ struct Command {
     args: ArgSignature,
 }
 
+impl Command {
+    // need a method for listing or iterating through all required options
+    fn parse(&self, params: &[String]) -> bool {
+
+    }
+}
+
 // An *option* has a name, zero or more arguments, and a multiplicity
 // which dictates how many times the option can/must show up.
 struct Opt {
@@ -28,6 +35,31 @@ struct Opt {
     narg: uint,
     types: ArgSignature,
     multiplicity: OptMult,
+}
+
+impl Opt {
+    fn check(&self, v: &[String]) -> bool {
+        if self.names != v[0] {
+            false
+        } else {
+            self.match_args(v.slice_from(1))
+        }
+    }
+
+    // checks whether a vector of strings can serve as the option's arguments
+    fn match_args(&self, v: &[String]) -> bool {
+        if self.narg != v.len() {
+            return false;
+        }
+
+        for (ty, s) in self.types.iter().zip( v.iter() ) {
+            if !ty.check(s) {
+                return false;
+            }
+        }
+        true
+    }
+
 }
 
 struct OptMult {
@@ -39,6 +71,16 @@ enum ParamVal {
     ParamStr,
     ParamInt,
     ParamBool,
+}
+
+impl ParamVal {
+    fn check(&self, s: String) -> bool {
+        match *self {
+            ParamStr => true,
+            ParamInt => { let x: int = from_str(s); x.is_some() },
+            ParamBool =>{ let x: bool = from_str(s); x.is_some() },
+        }
+    }
 }
 
 type ArgSignature = Vec<ParamVal>;
